60699b6fedb0bfb1ad40112df55f2845b094e756
Chris Beard <wbeard@mozilla.com>
2020-02-18 19:10:21 +0000

    new tables in bash script

--- bin/upload_plot.sh
+++ bin/upload_plot.sh
@@ -9,19 +9,21 @@ echo "Using project_dir: $project_dir"
 export PYTHONPATH="$project_dir:$PYTHONPATH"
 cd $project_dir
 
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate uptake_curves
+
 echo "=> Updating summary table"
 python uptake/upload_bq.py \
     --table_name='uptake_version_counts' \
     --sub_date_end=1 \
     --cache=True
 
-exit
 echo "=> Updating plot summary table"
 python uptake/plot/plot_upload.py \
     --sub_date=None \
-    --dest_table="analysis.wbeard_uptake_plot_test" \
-    --src_table="analysis.wbeard_uptake_vers" \
-    --project_id="moz-fx-data-derived-datasets" \
+    --dest_table="wbeard.uptake_plot_data" \
+    --src_table="wbeard.uptake_version_counts" \
+    --project_id="moz-fx-data-bq-data-science" \
     --cache=False \
     --creds_loc=None \
     --ret_df=None
@@ -29,6 +31,6 @@ python uptake/plot/plot_upload.py \
 echo "=> Generating plots"
 python uptake/plot/embed_html.py \
     --sub_date=None \
-    --plot_table="analysis.wbeard_uptake_plot_test" \
+    --plot_table="wbeard.uptake_plot_data" \
     --release_beta_nightly_n_versions="12,18,30" \
     --cache=True
