27cd7cacca32dcb64a40ee3418f6ff7676a610a0
wcbeard <wcbeard10@gmail.com>
2020-04-29 17:36:42 -0400

    prod upload_plot settings

--- README.md
+++ README.md
@@ -42,7 +42,38 @@ runs
 - `uptake/plot/embed_html.py`
 
 
+## Tests
+* `bin/test_upload.sh`
+* `bin/test_embed.sh`
 
+# GCP
+## To fire it up
+along lines of `gcp.sh`:
+
+```sh
+INSTANCE="wbeard-uptake"
+PROJECT="moz-fx-data-bq-data-science"
+ZONE="us-east1-b"
+
+gcloud beta compute instances start "wbeard-uptake" --project="moz-fx-data-bq-data-science" --zone="us-east1-b"
+
+gcloud beta compute \
+        ssh "$INSTANCE" \
+        --project $PROJECT \
+        --zone=$ZONE
+
+cd /home/wbeard/repos/uptake_curves
+
+        --command "cd /home/wbeard/repos/uptake_curves ;
+         rm -rf logfile ; rm -r "$GCP_OUT_CHANNEL_DIR/*" ; bash bin/upload_plot.sh 2>&1 |
+         tee logfile"
+```
+
+## Through web interface
+* Go to [link](https://console.cloud.google.com/cloud-resource-manager?project=&folder=&organizationId=442341870013)
+    * Hamburger -> Compute engine -> VM instances
+    * select wbeard-uptake-curve
+    * click instance-1 -> start
 
 # Workflows
 ## On update pipeline
--- bin/upload_plot.sh
+++ bin/upload_plot.sh
@@ -13,28 +13,40 @@ source ~/miniconda3/etc/profile.d/conda.sh
 conda activate uptake_curves
 export BQ_PROJ="moz-fx-data-bq-data-science"
 
-# echo "=> Updating summary table"
-# python uptake/upload_bq.py \
-#     --table_name='uptake_version_counts' \
-#     --sub_date_end=1 \
-#     --cache=True
+# Test/Prod
+export COUNT_TABLE="uptake_version_counts_test"
+export PLOT_TABLE="uptake_plot_data_test"
+CACHE="True"
 
-# echo "=> Updating plot summary table"
-# python uptake/plot/plot_upload.py \
-#     --sub_date=None \
-#     --dest_table="wbeard.uptake_plot_data" \
-#     --src_table="wbeard.uptake_version_counts" \
-#     --project_id="moz-fx-data-bq-data-science" \
-#     --cache=False \
-#     --creds_loc=None \
-#     --ret_df=None
+
+echo "=> Updating summary table"
+python uptake/upload_bq.py \
+    --table_name=$COUNT_TABLE \
+    --sub_date_end=1 \
+    --project_id=$BQ_PROJ \
+    --cache=True
+
+
+echo "=> Updating plot summary table"
+python uptake/plot/plot_upload.py \
+    --sub_date=None \
+    --src_table="wbeard.$COUNT_TABLE" \
+    --dest_table="wbeard.$PLOT_TABLE" \
+    --project_id=$BQ_PROJ \
+    --src_project_id=$BQ_PROJ \
+    --cache=$CACHE \
+    --creds_loc=None \
+    --ret_df=None
+
 
 echo "=> Generating plots"
 full_html_output=$(python uptake/plot/embed_html.py \
     --sub_date=None \
-    --plot_table="wbeard.uptake_plot_data" \
-    --release_beta_nightly_n_versions="12,18,30" \
-    --cache=True)
+    --plot_table="wbeard.$PLOT_TABLE" \
+    --project_id=$BQ_PROJ \
+    --release_beta_dev_nightly_n_versions="12,18,18,30" \
+    --cache=$CACHE)
+
 
 outdir=$(echo "$full_html_output" | tail -n 1)
 echo "outdir $outdir"
